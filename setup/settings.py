138845924616a921363e763e26b61854cce4b7a5
Isabelle <isabellebussmann@hotmail.com>
2021-07-07 17:44:00 -0300

    chore(settings): add static_root to settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 MEDIA_URL = '/media/'
 
