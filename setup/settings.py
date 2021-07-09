20bfc817816a99353c7d85feb97249f9e790bf88
Isabelle <isabellebussmann@hotmail.com>
2021-07-08 22:41:28 -0300

    chore(settings): add base configuration to heroku

--- setup/settings.py
+++ setup/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -175,3 +176,5 @@ REST_FRAMEWORK = {
 #
 # SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 # SESSION_CACHE_ALIAS = 'default'
+
+django_on_heroku.settings(locals())
